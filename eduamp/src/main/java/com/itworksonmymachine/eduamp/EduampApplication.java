e588602209e291121b367ad8a9bc5070abe8827b
suvoonhou <voonhousu@gmail.com>
2020-02-26 01:14:42 +0800

    [WIP] Google code style standardization

--- eduamp/src/main/java/com/itworksonmymachine/eduamp/EduampApplication.java
+++ eduamp/src/main/java/com/itworksonmymachine/eduamp/EduampApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EduampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EduampApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EduampApplication.class, args);
+  }
 
 }
