f1eb6c10bea6ebbf37797b7271977ce49d58862e
suvoonhou <voonhousu@gmail.com>
2020-02-26 00:45:55 +0800

    [WIP] Initialising project

---
+++ src/main/java/com/itworksonmymachine/eduamp/EduampApplication.java
@@ -0,0 +1,13 @@
+package com.itworksonmymachine.eduamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduampApplication.class, args);
+	}
+
+}
