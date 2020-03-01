ac1378fa0d610008cabc7ca612317f553f749616
bryanscw <38095231+bryanscw@users.noreply.github.com>
2020-03-01 15:37:38 +0800

    [SYNC] Resolved inter branch bugs (#11)
    * [WIP] Changed springframework version, added oauth, jdbc and jwt dependencies
    
    * [WIP] Adding parent pom.xml
    
    * [WIP] Initial OAuth2 and JWT implementation
    
    * [WIP] Checkpoint progress
    
    * [WIP] Working implementation of authentication and authorization
    
    * [WIP] Removed UserTokenSession
    
    * [WIP] Sync changes
    
    * [WIP] Tests can now be found by mvn test
    
    * [WIP] Added adoc samples
    
    * [WIP] Re-enabled disabled code
    
    * Bug fix
    
    Co-authored-by: voonhous <voonhousu@gmail.com>

---
+++ eduamp/src/main/java/com/itworksonmymachine/eduamp/model/Order.java
@@ -0,0 +1,22 @@
+package com.itworksonmymachine.eduamp.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "ORDER_TB")
+public class Order {
+  @Id
+  private int orderId;
+  private String name;
+  private int quantity;
+  private double price;
+}
--- eduamp/src/test/java/com/itworksonmymachine/eduamp/e2e/HelloMvcTest.java
+++ eduamp/src/test/java/com/itworksonmymachine/eduamp/e2e/HelloMvcTest.java
@@ -5,9 +5,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.itworksonmymachine.eduamp.config.TestConfig;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -22,6 +24,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
     classes = TestConfig.class
 )
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 public class HelloMvcTest {
 
   @Autowired
@@ -50,4 +53,5 @@ public class HelloMvcTest {
         .accept(MediaType.ALL))
         .andExpect(status().isUnauthorized());
   }
+
 }
