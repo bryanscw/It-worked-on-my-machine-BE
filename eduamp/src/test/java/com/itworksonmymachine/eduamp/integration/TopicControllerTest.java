f8a7a1ffe0ea6c9a2ea12b18a2315e08cd5935ab
suvoonhou <voonhousu@gmail.com>
2020-03-08 18:37:54 +0800

    [WIP] Removed redundant annotation 2

--- eduamp/src/test/java/com/itworksonmymachine/eduamp/integration/TopicControllerTest.java
+++ eduamp/src/test/java/com/itworksonmymachine/eduamp/integration/TopicControllerTest.java
@@ -23,7 +23,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
