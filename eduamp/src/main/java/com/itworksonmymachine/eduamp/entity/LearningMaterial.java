8a04ab5b88a64589cca38d1317ed18dee0907ed6
suvoonhou <voonhousu@gmail.com>
2020-03-16 22:39:24 +0800

    [WIP] Adding title and description into GameMap

--- eduamp/src/main/java/com/itworksonmymachine/eduamp/entity/LearningMaterial.java
+++ eduamp/src/main/java/com/itworksonmymachine/eduamp/entity/LearningMaterial.java
@@ -1,6 +1,7 @@
 package com.itworksonmymachine.eduamp.entity;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -43,6 +44,7 @@ public class LearningMaterial extends Auditable<String> {
 
   @Getter
   @Setter
+  @Column(columnDefinition = "TEXT")
   private String description;
 
 }
