6b030e2a9a0fab479bbec03acfc08174dbca22f2
suvoonhou <voonhousu@gmail.com>
2020-03-18 14:21:05 +0800

    [WIP] Adding more allowed headers for CORS filtering

--- eduamp/src/main/java/com/itworksonmymachine/eduamp/config/SimpleCorsFilter.java
+++ eduamp/src/main/java/com/itworksonmymachine/eduamp/config/SimpleCorsFilter.java
@@ -27,7 +27,7 @@ public class SimpleCorsFilter implements Filter {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PATCH, PUT");
     response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
       response.setStatus(HttpServletResponse.SC_OK);
