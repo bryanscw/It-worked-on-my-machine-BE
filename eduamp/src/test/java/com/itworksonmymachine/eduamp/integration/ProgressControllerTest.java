95d2e8f5f24335ce35295b863a1c08302f00fe55
bryanscw <csin001@e.ntu.edu.sg>
2020-03-17 23:57:01 -0400

    [WIP] Progress and User Controller Tests

--- eduamp/src/test/java/com/itworksonmymachine/eduamp/integration/ProgressControllerTest.java
+++ eduamp/src/test/java/com/itworksonmymachine/eduamp/integration/ProgressControllerTest.java
@@ -1,10 +1,30 @@
 package com.itworksonmymachine.eduamp.integration;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itworksonmymachine.eduamp.config.MockUserClass;
 import com.itworksonmymachine.eduamp.config.TestConfig;
+import com.itworksonmymachine.eduamp.entity.GameMap;
+import com.itworksonmymachine.eduamp.entity.Progress;
+import com.itworksonmymachine.eduamp.entity.Topic;
+import com.itworksonmymachine.eduamp.entity.User;
+import com.itworksonmymachine.eduamp.model.Coordinates;
+import com.itworksonmymachine.eduamp.repository.GameMapRepository;
+import com.itworksonmymachine.eduamp.repository.ProgressRepository;
+import com.itworksonmymachine.eduamp.repository.TopicRepository;
+import com.itworksonmymachine.eduamp.repository.UserRepository;
+import javax.swing.text.StyledEditorKit.ItalicAction;
 import javax.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -34,4 +54,320 @@ public class ProgressControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  private TopicRepository topicRepository;
+
+  @Autowired
+  private GameMapRepository gameMapRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private ProgressRepository progressRepository;
+
+  private MockUserClass user;
+
+  private Progress progress;
+
+  public Topic getPersistentTopic(){
+    Iterable<Topic> allTopics = topicRepository.findAll();
+    return allTopics.iterator().next();
+  }
+
+  public GameMap getPersistentGameMap(){
+    Iterable<GameMap> allGameMaps = gameMapRepository.findAll();
+    return allGameMaps.iterator().next();
+  }
+
+  public User getPersistentUser(){
+    Iterable<User> allUsers = userRepository.findAll();
+    return allUsers.iterator().next();
+  }
+
+  public Progress getPersistentProgress(){
+    Iterable<Progress> allProgress = progressRepository.findAll();
+    return allProgress.iterator().next();
+  }
+
+  @BeforeEach
+  public void setUp(){
+    Coordinates coordinates = new Coordinates();
+    coordinates.setX(1);
+    coordinates.setY(1);
+
+    this.progress = new Progress();
+    this.progress.setPosition(coordinates);
+  }
+  
+  @Test
+  @Order(1)
+  @WithUserDetails("teacher1@test.com")
+  public void createGameMap() throws Exception {
+    Topic topic = new Topic();
+    topic.setTitle("[Topic Title]: Multiplication");
+    topic.setDescription("[Topic Description]: This topic is about simple multiplication");
+
+    // We want principal information to be saved, do not use TopicRepository directly
+    String topicJson = new ObjectMapper().writeValueAsString(topic);
+    mockMvc.perform(
+        MockMvcRequestBuilders.post("/topics/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(topicJson))
+        .andExpect(status().isOk())
+        .andDo(document("{methodName}",
+        preprocessRequest(prettyPrint()),
+        preprocessResponse(prettyPrint())));
+
+    GameMap gameMap = new GameMap();
+    gameMap.setTitle("Game map title");
+    gameMap.setDescription("Test description");
+    gameMap.setMapDescriptor("Test map description");
+    gameMap.setPlayable(true);
+
+    String gameMapJson = new ObjectMapper().writeValueAsString(gameMap);
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .post(String.format("/topics/%s/gameMaps/create", getPersistentTopic().getId()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(gameMapJson))
+        .andExpect(status().isOk())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(2)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_allowCreateProgress_ifAuthorized() throws Exception {
+
+    String progressJson = new ObjectMapper().writeValueAsString(this.progress);
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .post(String.format("/progress/users/%s/gameMaps/%s",
+                "student1@test.com", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(progressJson))
+        .andExpect(status().isOk())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(3)
+  @Test
+  @WithUserDetails("user1@test.com")
+  @Transactional
+  public void should_rejectCreateProgress_ifNotAuthorized() throws Exception {
+
+    String progressJson = new ObjectMapper().writeValueAsString(this.progress);
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .post(String.format("/progress/users/%s/gameMaps/%s",
+                "user1@test.com", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(progressJson))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(4)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_allowFetchProgressByUserEmail_ifAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/users/%s", "student1@test.com"))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.position[\"x\"]", is(this.progress.getPosition().getX())))
+        .andExpect(jsonPath("$.position[\"y\"]", is(this.progress.getPosition().getY())))
+        .andExpect(jsonPath("$.user", is(getPersistentProgress().getUser())))
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(5)
+  @Test
+  @WithUserDetails("user1@test.com")
+  @Transactional
+  public void should_rejectFetchProgressByUserEmail_ifNotAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/users/%s", "user1@test.com"))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(6)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_allowFetchProgressByGameMapId_ifAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/gameMaps/%s", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.position[\"x\"]", is(this.progress.getPosition().getX())))
+        .andExpect(jsonPath("$.position[\"y\"]", is(this.progress.getPosition().getY())))
+        .andExpect(jsonPath("$.user", is(getPersistentProgress().getUser())))
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(7)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_rejectFetchProgressByGameMapId_ifNotExists() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/gameMaps/%s", getPersistentGameMap().getId()-1))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(8)
+  @Test
+  @WithUserDetails("user1@test.com")
+  @Transactional
+  public void should_rejectFetchProgressByGameMapId_ifNotAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/gameMaps/%s", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(9)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_allowFetchProgressByUserEmailAndGameMapId_ifAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/users/%s/gameMaps/%s",
+                "student1@test.com", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.position[\"x\"]", is(this.progress.getPosition().getX())))
+        .andExpect(jsonPath("$.position[\"y\"]", is(this.progress.getPosition().getY())))
+        .andExpect(jsonPath("$.user", is(getPersistentProgress().getUser())))
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(10)
+  @Test
+  @WithUserDetails("user1@test.com")
+  @Transactional
+  public void should_rejectFetchProgressByUserEmailAndGameMapId_ifNotAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/users/%s/gameMaps/%s",
+                "user1@test.com", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(11)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_rejectFetchProgressByUserEmailAndGameMapId_ifGameMapNotExists() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/users/%s/gameMaps/%s",
+                "student1@test.com", getPersistentGameMap().getId()-1))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(12)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_rejectFetchProgressByUserEmailAndGameMapId_ifUserEmailNotExists() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .get(String.format("/progress/users/%s/gameMaps/%s",
+                "student0@test.com", getPersistentGameMap().getId()-1))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(13)
+  @Test
+  @WithUserDetails("student1@test.com")
+  @Transactional
+  public void should_allowUpdateProgress_ifAuthorized() throws Exception {
+
+    this.progress.setComplete(true);
+
+    String progressJson = new ObjectMapper().writeValueAsString(this.progress);
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .put(String.format("/progress/users/%s/gameMaps/%s",
+                "student1@test.com", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(progressJson))
+        .andExpect(status().isNotFound())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(14)
+  @Test
+  @WithUserDetails("user1@test.com")
+  @Transactional
+  public void should_rejectUpdateProgress_ifNotAuthorized() throws Exception {
+
+    this.progress.setComplete(true);
+
+    String progressJson = new ObjectMapper().writeValueAsString(this.progress);
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .put(String.format("/progress/users/%s/gameMaps/%s",
+                "user1@test.com", getPersistentGameMap().getId()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(progressJson))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+
+    progressRepository.deleteById(getPersistentProgress().getId());
+    userRepository.deleteUserByEmail(getPersistentUser().getEmail());
+    gameMapRepository.deleteById(getPersistentGameMap().getId());
+    topicRepository.deleteById(getPersistentTopic().getId());
+  }
 }
+
--- eduamp/src/test/java/com/itworksonmymachine/eduamp/integration/UserControllerTest.java
+++ eduamp/src/test/java/com/itworksonmymachine/eduamp/integration/UserControllerTest.java
@@ -12,8 +12,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itworksonmymachine.eduamp.config.MockUserClass;
 import com.itworksonmymachine.eduamp.config.TestConfig;
+import com.itworksonmymachine.eduamp.entity.User;
+import com.itworksonmymachine.eduamp.repository.UserRepository;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
@@ -37,7 +41,15 @@ public class UserControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  private UserRepository userRepository;
+
   private MockUserClass user;
+
+  public User getPersistentUser() {
+    Iterable<User> allUsers = userRepository.findAll();
+    return allUsers.iterator().next();
+  }
 
   @BeforeEach
   private void setup() {
@@ -48,11 +60,12 @@ public class UserControllerTest {
     this.user.setRole("ROLE_STUDENT");
   }
 
+  @Order(1)
   @Test
   @WithUserDetails("teacher1@test.com")
   public void should_rejectCreate_ifNotAuthorized() throws Exception {
     String userJson = new ObjectMapper().writeValueAsString(this.user);
-    this.mockMvc.perform(post("/users/create")
+    mockMvc.perform(post("/users/create")
         .contentType(MediaType.APPLICATION_JSON)
         .content(userJson))
         .andExpect(status().isForbidden())
@@ -61,12 +74,12 @@ public class UserControllerTest {
             preprocessResponse(prettyPrint())));
   }
 
+  @Order(2)
   @Test
   @WithUserDetails("admin1@test.com")
-  @Transactional
   public void should_allowCreate_ifAuthorized() throws Exception {
     String userJson = new ObjectMapper().writeValueAsString(this.user);
-    this.mockMvc.perform(post("/users/create")
+    mockMvc.perform(post("/users/create")
         .contentType(MediaType.APPLICATION_JSON)
         .content(userJson))
         .andExpect(status().isOk())
@@ -76,20 +89,15 @@ public class UserControllerTest {
             preprocessResponse(prettyPrint())));
   }
 
+  @Order(3)
   @Test
   @WithUserDetails("admin1@test.com")
   @Transactional
   public void should_rejectCreate_ifUserAlreadyExists() throws Exception {
     String userJson = new ObjectMapper().writeValueAsString(this.user);
 
-    // Create a user
-    this.mockMvc.perform(post("/users/create")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(userJson))
-        .andExpect(status().isOk());
-
     // Add a user that already exists
-    this.mockMvc.perform(post("/users/create")
+    mockMvc.perform(post("/users/create")
         .contentType(MediaType.APPLICATION_JSON)
         .content(userJson))
         .andExpect(status().isBadRequest())
@@ -98,4 +106,137 @@ public class UserControllerTest {
             preprocessResponse(prettyPrint())));
   }
 
+  @Order(4)
+  @Test
+  @WithUserDetails("admin1@test.com")
+  @Transactional
+  public void should_allowFetchUsers_ifAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders.get("/users/")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.content[0].email", is(this.user.getEmail())))
+        .andExpect(jsonPath("$.content[0].name", is(this.user.getName())))
+        .andExpect(jsonPath("$.content[0].role", is(this.user.getRole())))
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(5)
+  @Test
+  @WithUserDetails("teacher1@test.com")
+  @Transactional
+  public void should_rejectFetchUsers_ifNotAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders.get("/users/")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(6)
+  @Test
+  @WithUserDetails("teacher1@test.com")
+  @Transactional
+  public void should_rejectUpdateUser_ifNotAuthorized() throws Exception {
+    this.user.setEmail("newEmail@test.com");
+
+    String userJson = new ObjectMapper().writeValueAsString(this.user);
+    mockMvc.perform(
+        MockMvcRequestBuilders.put(String.format("/users/%s", getPersistentUser().getEmail()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(userJson))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(7)
+  @Test
+  @WithUserDetails("admin1@test.com")
+  @Transactional
+  public void should_rejectUpdateUser_ifNotExists() throws Exception {
+    this.user.setEmail("newEmail@test.com");
+
+    String userJson = new ObjectMapper().writeValueAsString(this.user);
+    mockMvc.perform(
+        MockMvcRequestBuilders.put(String.format("/users/%s", "user1@test.com"))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(userJson))
+        .andExpect(status().isNotFound())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(8)
+  @Test
+  @WithUserDetails("admin1@test.com")
+  @Transactional
+  public void should_allowUpdateUser_ifAuthorized() throws Exception {
+
+    this.user.setName("new name");
+
+    String userJson = new ObjectMapper().writeValueAsString(this.user);
+    mockMvc.perform(
+        MockMvcRequestBuilders.put(String.format("/users/%s", getPersistentUser().getEmail()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(userJson))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.email", is(this.user.getEmail())))
+        .andExpect(jsonPath("$.name", is(this.user.getName())))
+        .andExpect(jsonPath("$.role", is(this.user.getRole())))
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(9)
+  @Test
+  @WithUserDetails("teacher1@test.com")
+  @Transactional
+  public void should_rejectDeleteUser_ifNotAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders.delete(String.format("/users/%s", getPersistentUser().getEmail()))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
+  @Order(10)
+  @Test
+  @WithUserDetails("admin1@test.com")
+  @Transactional
+  public void should_allowDeleteUser_ifAuthorized() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders.delete(String.format("/users/%s", this.user.getEmail()))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+    userRepository.deleteUserByEmail(getPersistentUser().getEmail());
+  }
+
+  @Order(11)
+  @Test
+  @WithUserDetails("admin1@test.com")
+  @Transactional
+  public void should_rejectDeleteUser_ifNotExists() throws Exception {
+    mockMvc.perform(
+        MockMvcRequestBuilders.delete(String.format("/users/%s", this.user.getEmail()))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isForbidden())
+        .andDo(document("{methodName}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint())));
+  }
+
 }
+
